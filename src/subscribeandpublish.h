21d9d86e6c4ed5e53f83df88ef1912ade7f1897d
Karl Kurzer <karl.kurzer@outlook.com>
2016-01-05 16:43:22 +0100

    added yaw angle conversion

--- src/subscribeandpublish.h
+++ src/subscribeandpublish.h
@@ -4,6 +4,9 @@
 #include <iostream>
 
 #include <ros/ros.h>
+#include <tf/transform_datatypes.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
 
 #include "node3d.h"
 #include "path.h"
@@ -17,6 +20,8 @@ class SubscribeAndPublish {
     sub_map = n.subscribe("/map", 1, &SubscribeAndPublish::setMap, this);
     sub_goal = n.subscribe("/move_base_simple/goal", 1,
                            &SubscribeAndPublish::setGoal, this);
+    sub_start = n.subscribe("/initialpose", 1,
+                            &SubscribeAndPublish::setStart, this);
   }
 
   void setMap(const nav_msgs::OccupancyGrid::ConstPtr map) {
@@ -27,19 +32,40 @@ class SubscribeAndPublish {
   void setGoal(const geometry_msgs::PoseStamped::ConstPtr& goal) {
     int x = (int)goal->pose.position.x;
     int y = (int)goal->pose.position.y;
-    float t = goal->pose.orientation.z;
-    float w = goal->pose.orientation.w;
-    std::cout << "I am seeing a new goal x:" << x << " y:" << y
-              << " t:" << 2 * asin(t) << " w:" << 2 * acos(w) << std::endl;
-    Node3D nStart(0, 0, 180, 0, 0, nullptr);
+    float t = tf::getYaw(goal->pose.orientation);
 
     if (grid->info.height >= y && y >= 0 && grid->info.width >= x && x >= 0) {
-      Node3D nGoal(x, y, 180, 0, 0, nullptr);
+      Node3D nGoal(x, y, t * 180 / 3.14, 0, 0, nullptr);
+      Node3D nStart(0, 0, 0, 0, 0, nullptr);
+
+      if (start != nullptr) {
+        nStart.setX(start->pose.pose.position.x);
+        nStart.setY(start->pose.pose.position.y);
+        nStart.setT(tf::getYaw(start->pose.pose.orientation) * 180 / 3.14);
+      }
+
+      std::cout << "I am seeing a new goal x:" << x << " y:" << y << " t:"
+                << t << " deg:" << t * 180 / 3.14 << std::endl;
       Path path(Node3D::aStar(nStart, nGoal, grid), "path");
       pub_path.publish(path.getPath());
     } else {
-      std::cout << "Invalid goal selected x:" << x << " y:" << y
-                << " t:" << 2 * asin(t) << " w:" << 2 * acos(w) << std::endl;
+      std::cout << "Invalid goal selected x:" << x << " y:" << y << " t:"
+                << t << " deg:" << t * 180 / 3.14 << std::endl;
+    }
+  }
+
+  void setStart(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& initial) {
+    int x = (int)initial->pose.pose.position.x;
+    int y = (int)initial->pose.pose.position.y;
+    float t = tf::getYaw(initial->pose.pose.orientation);
+    std::cout << "I am seeing a new start x:" << x << " y:" << y << " t:"
+              << t << " deg:" << t * 180 / 3.14 << std::endl;
+
+    if (grid->info.height >= y && y >= 0 && grid->info.width >= x && x >= 0) {
+      start = initial;
+    } else {
+      std::cout << "Invalid start selected x:" << x << " y:" << y << " t:"
+                << t << " deg:" << t * 180 / 3.14 << std::endl;
     }
   }
 
@@ -48,7 +74,9 @@ class SubscribeAndPublish {
   ros::Publisher pub_path;
   ros::Subscriber sub_map;
   ros::Subscriber sub_goal;
+  ros::Subscriber sub_start;
   nav_msgs::OccupancyGrid::ConstPtr grid;
+  geometry_msgs::PoseWithCovarianceStamped::ConstPtr start;
 };
 
 #endif // SUBSCRIBEANDPUBLISH
